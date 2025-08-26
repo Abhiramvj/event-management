202885e8e9a55f4b725baa5e748c0ac1908dd2ac
Abhiramvj <abhiramsanthosh2@gmail.com>
2025-08-26 19:11:30 +0530

    edited styles

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./*.html", // Scans all HTML files in the root
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
