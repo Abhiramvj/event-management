71930e192afced6d8b82568806af3842b4b4d6d2
Abhiramvj <abhiramsanthosh2@gmail.com>
2025-09-09 02:29:17 +0530

    Added some changes

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,22 @@
 import { defineConfig } from 'vite'
+import { resolve } from 'path'
 import tailwindcss from '@tailwindcss/vite'
+
 export default defineConfig({
   plugins: [
     tailwindcss(),
   ],
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+        about: resolve(__dirname, 'about.html'),
+        contact: resolve(__dirname, 'contact.html'),
+        services: resolve(__dirname, 'services.html'),
+        gallery: resolve(__dirname, 'gallery.html'),
+        footer: resolve(__dirname, 'footer.html'),
+        header: resolve(__dirname, 'header.html'),
+      },
+    },
+  },
 })
